4190b3f4ce65dd17c0f786bdde43073fd03a7a92
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-06-19 23:30:06 -0300

    Componente Navegação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,19 +7,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component'
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule} from '@angular/material/sidenav'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
